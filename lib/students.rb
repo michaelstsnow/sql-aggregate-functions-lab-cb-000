d2e82695b4b256a17a871a6b9ef500863f262eb6
michaelstsnow <michael.st.snow@gmail.com>
2019-05-26 18:42:11 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
-  max_gpa=SELECT max(gpa) FROM students;
+  max_gpa=SELECT max(gpa) from students;
   max_gpa
 end
 
