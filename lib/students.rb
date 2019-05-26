49a912784f3eb5f4d803f13d3a37797f16f133b0
michaelstsnow <michael.st.snow@gmail.com>
2019-05-26 18:41:06 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,17 +5,17 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  SELECT min(gpa) FROM students
+  #SELECT min(gpa) FROM students
 end
 
 def average_student_gpa
-  SELECT avg(gpa) FROM students
+  #SELECT avg(gpa) FROM students
 end
 
 def total_tardies_for_all_students
-  SELECT sum(tardies) FROM students
+  #SELECT sum(tardies) FROM students
 end
 
 def average_gpa_for_9th_grade
-  SELECT avg(gpa) FROM students WHERE grade = 9
+  SELECT avg(gpa) FROM students WHERE grade = 9;
 end
